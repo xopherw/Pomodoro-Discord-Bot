b09e98e70e90324f967e5b2f8f59979e90825b2a
Christopher Wong <31772094+xopherw@users.noreply.github.com>
2021-02-06 11:12:16 +0800

    Update pomodoro_bot.py

--- pomodoro_bot.py
+++ pomodoro_bot.py
@@ -1,36 +1,32 @@
-# import discord, time, asyncio, re, json, datetime as dt, os
-# from discord.ext import commands
 from pomodoro_func import *
 
 # create the "user.json" file with an assigned dummy value.
 open('user.json','w').write('{"dummy" : "dummy" }')
 
-while(True):
-    try:
-        client = discord.Client()
+client = discord.Client()
 
 #----------- @client.events -----------#
 
-        @client.event
-        async def on_message(message):      
+@client.event
+async def on_message(message):      
 
-            if (message.author == client.user): return
+    if (message.author == client.user): return
 
-            if (re.match(r"^(\!timer start)$", message.content)):
-                await userCheck(client, 15, message)    
-                
-            elif(re.match(r"^(\!timer start) [0-9]{1,2}$", message.content)):
-                period = int(message.content.split(' ')[-1])
-                userCheck(client, period, message)
-                
-            elif(message.content == "!timer help"): await message.channel.send(help)
+    if (re.match(r"^(\!timer start)$", message.content)):
+        await userCheck(client, 15, message)    
+        
+    elif(re.match(r"^(\!timer start) [0-9]{1,2}$", message.content)):
+        period = int(message.content.split(' ')[-1])
+        await userCheck(client, period, message)
+        
+    elif(message.content == "!timer help"): await message.channel.send(help)
 
-        @client.event
-        async def on_ready():
-            print(f'{client.user} has connected to Discord!')
-       
-        client.run("YOUR_TOKEN")
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
 
+while(True):
+    try:    client.loop.run_until_complete(client.run('TOKEN'))
     except Exception:
         print("Reconnecting, please hold...")
         time.sleep(5)
